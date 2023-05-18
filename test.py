c2a6fed365eb4fa75f826d42f1df0080756572ec
Pathos14489 <shinigamiking14489@gmail.com>
2023-05-18 12:39:18 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ booru = BooruPlus()
 
 # booru.scrape()  # scrape the booru and make sure it's up to date -- If it's up to date, this will only make 1 request to get the number of pages, 
                 # and then it will check every page to see if it's up to date. If it's not up to date, it will then only scrape the pages that are not up to date. 
-                # So 11 requests total for a 10 page booru. Could probably be slightly more efficient, but don't give enough of a fuck.
+                # So 11 requests total for a 10 page booru. Could probably be slightly more efficient, but don't care.
